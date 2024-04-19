92d7094befd913f12d9027e835c92897421c9c07
jonnysjf <jonnys.gomes@outlook.com>
2024-04-19 14:37:33 -0300

    new file runner-tools

---
+++ select-tool-find.py
@@ -0,0 +1,17 @@
+import hashlib
+from bit import Key
+from bit.format import bytes_to_wif
+import requests
+import json
+from itertools import starmap 
+from datetime import datetime, timedelta
+import pandas as pd
+from set_address import set_list_address
+from runner-tools import option
+   
+#APRESENTAÇÃO DO MENU PARA ESCOLHA     
+def main():
+      option(int(input("[1] Good Luck! \n[2] Find Duplicidade \n[3] Extrair Addresses (100.000)\n[4] Compilar fail's excluindo repetidos \n[0] Sair \n: ")))  
+
+if __name__ == '__main__':
+	main()
